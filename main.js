71a83956b04d7a96a69eb946beb8d7f482130c29
yuuta716 <139669904+yuuta716@users.noreply.github.com>
2024-09-05 21:12:53 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -111,6 +111,28 @@
 }
 
 {
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      };
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
+    });
+  };
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new
+    IntersectionObserver(callback, options);//場所
+  const targets = document.querySelectorAll(".mywork");//監視されるもの
+  targets.forEach(target => {
+    observer.observe(target);//obseverにtargetが入るときの.observe監視対象にする
+  });
+}
+
+{
   $(".openbtn1").click(function () {//ボタンがクリックされたら
     $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
     $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
