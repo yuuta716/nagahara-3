9106f43d1302284d9be8d53af08429fd6609a673
yuuta716 <139669904+yuuta716@users.noreply.github.com>
2024-09-07 11:10:04 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -145,33 +145,9 @@
 }
 
 {
-  document.addEventListener("DOMContentLoaded", function() {
-    const content1 = document.querySelector(".introduction");
-    const scrollThresholdStart = 100; // 変化が始まるスクロールの閾値
-    const scrollThresholdEnd = 100; // 変化が終わるスクロールの閾値
-  
-    window.addEventListener("scroll", function() {
-      const scrollPosition = window.scrollY;
-      const body = document.querySelector("body");
-      // スクロール位置に応じて背景色と文字色を変更
-      if (scrollPosition > (content1.offsetTop - scrollThresholdStart) 
-       && scrollPosition < (content1.offsetTop + content1.offsetHeight + scrollThresholdEnd)
-       )
-       {
-        body.style.backgroundColor = "black";
-        body.style.color = "white"; // 文字色を白に変更
-       } else {
-        body.style.backgroundColor = "white";
-        body.style.color = "black"; // 文字色を黒に変更
-      }
-    });
-  });
-}
-
-{
   const next = document.getElementById('next');
   const prev = document.getElementById('prev');
-  const ul = document.querySelector('ul');
+  const ul = document.querySelector('container ul');
   const slides = ul.children;
   const dots = [];
   let currentIndex = 0;
