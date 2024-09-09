3c64c33258c85457f5f9073243c936a89f1fde74
yuuta716 <139669904+yuuta716@users.noreply.github.com>
2024-09-09 13:32:48 +0900

    Add files via upload

--- performance.js
+++ performance.js
@@ -1,84 +1,90 @@
 "use strict";
 
 {
-  $(".openbtn1").click(function () {//ボタンがクリックされたら
-    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const menu = document.getElementById('menu');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
   });
 
-  $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  menu.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
   });
 }
 
 {
-  const next = document.getElementById('next');
-  const prev = document.getElementById('prev');
-  const ul = document.querySelector('ul');
-  const slides = ul.children;
-  const dots = [];
-  let currentIndex = 0;
-
-  function updateButtons() {
-    prev.classList.remove('hidden');
-    next.classList.remove('hidden');
-
-    if (currentIndex === 0) {
-      prev.classList.add('hidden');
-    }
-
-    if (currentIndex === slides.length - 1) {
-      next.classList.add('hidden');
-    }
-  }
-
-  function moveSlides() {
-    const slideWidth = slides[0].getBoundingClientRect().width;
-    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
-  };
-
-  function setupDots() {
-    for (let i = 0; i < slides.length; i++) {
-      const button = document.createElement('button');
-      button.addEventListener('click', () => {
-        currentIndex = i;
-        updateDots();
-        updateButtons();
-        moveSlides();
-      });
-      dots.push(button);
-      document.querySelector('nav').appendChild(button);
-    }
-
-    dots[0].classList.add('current');
-  }
-
-  function updateDots() {
-    dots.forEach(dot => {
-      dot.classList.remove('current');
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      };
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
     });
-    dots[currentIndex].classList.add('current');
+  };
+  const options = {
+    threshold: 0.2,
   };
 
-  updateButtons();
-  setupDots();
-
-  next.addEventListener('click', () => {
-    currentIndex++;
-    updateButtons();
-    updateDots();
-    moveSlides();
-  });
-
-  prev.addEventListener('click', () => {
-    currentIndex--;
-    updateButtons();
-    updateDots();
-    moveSlides();
-  });
-
-  window.addEventListener('resize', () => {
-    moveSlides();
+  const observer = new
+    IntersectionObserver(callback, options);//場所
+  const targets = document.querySelectorAll(".myabout");//監視されるもの
+  targets.forEach(target => {
+    observer.observe(target);//obseverにtargetが入るときの.observe監視対象にする
   });
 }
+
+{
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      };
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
+    });
+  };
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new
+    IntersectionObserver(callback, options);//場所
+  const targets = document.querySelectorAll(".myprofile");//監視されるもの
+  targets.forEach(target => {
+    observer.observe(target);//obseverにtargetが入るときの.observe監視対象にする
+  });
+}
+
+{
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      };
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
+    });
+  };
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new
+    IntersectionObserver(callback, options);//場所
+  const targets = document.querySelectorAll(".mycareer");//監視されるもの
+  targets.forEach(target => {
+    observer.observe(target);//obseverにtargetが入るときの.observe監視対象にする
+  });
+}
+
