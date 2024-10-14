28ca957b979351855feb957e9355b1b7269bce3b
yuuta716 <139669904+yuuta716@users.noreply.github.com>
2024-10-14 15:41:33 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,91 @@
 "use strict";
+
 {
-  // IE、Safari対応
-  // smoothscroll.js読み込み
-  // https://github.com/iamdustan/smoothscroll
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      };
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
+    });
+  };
+  const options = {
+    threshold: 0.2,
+  };
 
-  // セレクタ名（.pagetop）に一致する要素を取得
-  const pagetop_btn = document.querySelector(".pagetop");
+  const observer = new
+    IntersectionObserver(callback, options);//場所
+  const targets = document.querySelectorAll(".text-4");//監視されるもの
+  targets.forEach(target => {
+    observer.observe(target);//obseverにtargetが入るときの.observe監視対象にする
+  });
+}
 
-  // .pagetopをクリックしたら
-  pagetop_btn.addEventListener("click", scroll_top);
+{
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      };
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
+    });
+  };
+  const options = {
+    threshold: 0.2,
+  };
 
-  // ページ上部へスムーズに移動
-  function scroll_top() {
-    window.scroll({ top: 0, behavior: "smooth" });
-  }
+  const observer = new
+    IntersectionObserver(callback, options);//場所
+  const targets = document.querySelectorAll(".text-3");//監視されるもの
+  targets.forEach(target => {
+    observer.observe(target);//obseverにtargetが入るときの.observe監視対象にする
+  });
+}
 
-  // スクロールされたら表示
-  window.addEventListener("scroll", scroll_event);
-  function scroll_event() {
-    if (window.pageYOffset > 100) {
-      pagetop_btn.style.opacity = "1";
-    } else if (window.pageYOffset < 100) {
-      pagetop_btn.style.opacity = "0";
-    }
-  }
+{
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      };
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
+    });
+  };
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new
+    IntersectionObserver(callback, options);//場所
+  const targets = document.querySelectorAll(".text-2");//監視されるもの
+  targets.forEach(target => {
+    observer.observe(target);//obseverにtargetが入るときの.observe監視対象にする
+  });
+}
+
+{
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      };
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
+    });
+  };
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new
+    IntersectionObserver(callback, options);//場所
+  const targets = document.querySelectorAll(".text-1");//監視されるもの
+  targets.forEach(target => {
+    observer.observe(target);//obseverにtargetが入るときの.observe監視対象にする
+  });
 }
 
 {
@@ -153,8 +215,8 @@
   });
 
   menu.addEventListener('click', () => {
-    overlay.classList.remove('show');
-    open.classList.remove('hide');
+    overlay.classList.add('show');
+    open.classList.add('hide');
   });
 }
 
